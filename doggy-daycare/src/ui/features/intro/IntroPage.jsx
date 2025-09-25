ae9bb589fa09ee2cf2d18831f404e164788ef050
Magnus Freidenfelt <sabbathmagnus@gmail.com>
2025-09-25 15:42:42 +0200

    Added content to intro page

--- doggy-daycare/src/ui/features/intro/IntroPage.jsx
+++ doggy-daycare/src/ui/features/intro/IntroPage.jsx
@@ -1,10 +1,28 @@
 import "./IntroPage.css";
 
+import info from "./../../../assets/info.jpg";
+
 function IntroPage() {
   return (
-    <>
-      <h1>IntroPage</h1>
-    </>
+    <div className="intro">
+      <h1>Välkommen till Beatas hunddagis personalsida!</h1>
+
+      <p>Navigera sidan via knapparna ovan.</p>
+      <div className="row">
+        <p className="bold">Gäster: </p>
+        <p>
+          Här kan du se alla hundar som är kunder just nu. Klicka på en bild för
+          att se mer information.
+        </p>
+      </div>
+      <div className="row">
+        <p className="bold">Register:</p>
+        <p>Se alla nuvarande och tidigare kunder samt ägarinformation.</p>
+      </div>
+      <a href={info} target="_blank" rel="noopener noreferrer">
+        <img src={info} />
+      </a>
+    </div>
   );
 }
 
